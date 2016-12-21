84651614ddef03e94d546e336b04b69f3391d8e9
ppadalka <ppadalka@inbox.ru>
2016-12-21 22:32:03 +0200

    init spring boot project

---
+++ src/main/java/ua/ppadalka/webstore/WebStoreApplication.java
@@ -0,0 +1,12 @@
+package ua.ppadalka.webstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStoreApplication.class, args);
+	}
+}
---
+++ src/test/java/ua/ppadalka/webstore/WebStoreApplicationTests.java
@@ -0,0 +1,14 @@
+package ua.ppadalka.webstore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebStoreApplicationTests {
+
+	@Test
+	public void contextLoads() {}
+}
