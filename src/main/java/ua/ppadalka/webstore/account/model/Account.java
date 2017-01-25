d1cb3dfb58cbd59c6768c38a01b1f139177c7007
ppadalka <ppadalka@inbox.ru>
2017-01-26 01:09:40 +0200

    Added  some field for account entity

--- src/main/java/ua/ppadalka/webstore/account/model/Account.java
+++ src/main/java/ua/ppadalka/webstore/account/model/Account.java
@@ -1,47 +1,111 @@
 package ua.ppadalka.webstore.account.model;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
 
 @Entity
+@Table(name = "ACCOUNT")
+@SequenceGenerator(
+        name = "ACCOUNT_ID_SEQUENCE_GENERATOR",
+        sequenceName = "ACCOUNT_ID_SEQUENCE",
+        allocationSize = 1
+)
 public class Account implements UserDetails, Serializable {
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+    @Id
+    @GeneratedValue(
+            generator = "ACCOUNT_ID_SEQUENCE_GENERATOR",
+            strategy = GenerationType.SEQUENCE
+    )
+    @Column(name = "ACCOUNT_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "ROLE")
+    private Role role;
+
+    public Account(String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
     }
 
     @Override
-    public String getPassword() {
-        return null;
+    public int hashCode() {
+        return Objects.hash(username, role);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (!(obj instanceof Account)) return false;
+
+        Account account = (Account) obj;
+
+        return Objects.equals(username, account.username) &&
+                Objects.equals(role, account.role);
+    }
+
+    public Long getId() {
+        return id;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        GrantedAuthority authority = new SimpleGrantedAuthority(role.name());
+        return Collections.singletonList(authority);
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
