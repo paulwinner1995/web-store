a45a20318c7b198e2a110d0452e1c0cc9f5c195e
ppadalka <ppadalka@inbox.ru>
2017-01-28 21:00:30 +0200

    added mock implementation for password encoder

--- src/main/java/ua/ppadalka/webstore/security/config/SecurityConfiguration.java
+++ src/main/java/ua/ppadalka/webstore/security/config/SecurityConfiguration.java
@@ -1,3 +1,29 @@
 package ua.ppadalka.webstore.security.config;
 
-public class SecurityConfiguration {}
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Objects;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // TODO: Temporary implementation of Password encoder
+        // TODO: Should be replaced with bcrypt encoder later
+        return new PasswordEncoder() {
+
+            @Override
+            public String encode(CharSequence rawPassword) {
+                return rawPassword.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                return Objects.equals(rawPassword.toString(), encodedPassword);
+            }
+        };
+    }
+}
