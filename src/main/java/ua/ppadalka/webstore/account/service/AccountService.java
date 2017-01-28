5367dd37c68f023fc5f5cd88ed32129325a6afa0
ppadalka <ppadalka@inbox.ru>
2017-01-28 21:47:06 +0200

    added create account method

--- src/main/java/ua/ppadalka/webstore/account/service/AccountService.java
+++ src/main/java/ua/ppadalka/webstore/account/service/AccountService.java
@@ -1,7 +1,18 @@
 package ua.ppadalka.webstore.account.service;
 
-import org.springframework.stereotype.Service;
+import ua.ppadalka.webstore.account.model.Account;
+import ua.ppadalka.webstore.account.model.Role;
 
-@Service
 public interface AccountService {
+
+    /**
+     * Creates account with given username, password and role
+     *
+     * @param username - username for new account
+     * @param password - raw password for new account (password will be encoded)
+     * @param role     - role for new account
+     *
+     * @return - created account
+     */
+    Account create(String username, String password, Role role);
 }
--- src/main/java/ua/ppadalka/webstore/account/service/AccountServiceImpl.java
+++ src/main/java/ua/ppadalka/webstore/account/service/AccountServiceImpl.java
@@ -1,16 +1,39 @@
 package ua.ppadalka.webstore.account.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import ua.ppadalka.webstore.account.model.Account;
+import ua.ppadalka.webstore.account.model.Role;
 import ua.ppadalka.webstore.account.repository.AccountRepository;
+
+import java.time.LocalDateTime;
 
 @Service
 public class AccountServiceImpl implements AccountService {
 
-    private AccountRepository accountRepository;
+    private AccountRepository repository;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public AccountServiceImpl(AccountRepository accountRepository) {
-        this.accountRepository = accountRepository;
+    public AccountServiceImpl(AccountRepository repository, PasswordEncoder encoder) {
+        this.repository = repository;
+        this.passwordEncoder = encoder;
+    }
+
+    @Override
+    public Account create(String username, String password, Role role) {
+        String encodedPassword = passwordEncoder.encode(password);
+        LocalDateTime currentDate = LocalDateTime.now();
+
+        Account account = Account.builder()
+                .username(username)
+                .password(encodedPassword)
+                .role(role)
+                .registrationDate(currentDate)
+                .version(currentDate)
+                .build();
+
+        return repository.save(account);
     }
 }
