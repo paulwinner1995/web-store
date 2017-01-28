8c3ebf9493b6dcda78c0d076bd05f1d6ee17f548
ppadalka <ppadalka@inbox.ru>
2017-01-28 21:56:50 +0200

    added registration resource

--- src/main/java/ua/ppadalka/webstore/security/rest/RegistrationResource.java
+++ src/main/java/ua/ppadalka/webstore/security/rest/RegistrationResource.java
@@ -1,4 +1,28 @@
 package ua.ppadalka.webstore.security.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ua.ppadalka.webstore.security.dto.RegistrationForm;
+import ua.ppadalka.webstore.security.service.RegistrationService;
+
+@RestController
+@RequestMapping(path = "/registration")
 public class RegistrationResource {
+
+    private RegistrationService registrationService;
+
+    @Autowired
+    public RegistrationResource(RegistrationService service) {
+        this.registrationService = service;
+    }
+
+    @PostMapping
+    public ResponseEntity registerCustomer(@RequestBody RegistrationForm form) {
+        registrationService.registerCustomer(form);
+        return ResponseEntity.ok().build();
+    }
 }
