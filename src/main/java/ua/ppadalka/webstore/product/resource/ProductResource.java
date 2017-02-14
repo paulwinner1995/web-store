c81b85942a2575881c9766c38cd6da510f31cdf1
ppadalka <ppadalka@inbox.ru>
2017-02-14 22:26:38 +0200

    fixed compile error in ProductResource class

--- src/main/java/ua/ppadalka/webstore/product/resource/ProductResource.java
+++ src/main/java/ua/ppadalka/webstore/product/resource/ProductResource.java
@@ -2,8 +2,8 @@ package ua.ppadalka.webstore.product.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.method.P;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +22,8 @@ public class ProductResource {
     }
 
     @GetMapping
-    public ResponseEntity<Page<ProductDto>> findProductPage() {
-        Page<ProductDto> page = productService.findProductPage();
+    public ResponseEntity<Page<ProductDto>> findProductPage(Pageable pageable) {
+        Page<ProductDto> page = productService.findProductPage(pageable);
         return ResponseEntity.ok(page);
     }
 }
