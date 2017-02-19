25bde37dc0f6cb9d9671e3a2007a4276b82ed30a
ppadalka <ppadalka@inbox.ru>
2017-02-19 15:03:55 +0200

    added implementation for ProductInfoDto

--- src/main/java/ua/ppadalka/webstore/product/dto/ProductInfoDto.java
+++ src/main/java/ua/ppadalka/webstore/product/dto/ProductInfoDto.java
@@ -1,4 +1,54 @@
 package ua.ppadalka.webstore.product.dto;
 
+import java.util.List;
+
 public class ProductInfoDto {
+
+    private String name;
+    private Double price;
+    private String description;
+    private ProductCategoryDto category;
+    private List<ProductDetailDto> details;
+
+    public ProductInfoDto() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ProductCategoryDto getCategory() {
+        return category;
+    }
+
+    public void setCategory(ProductCategoryDto category) {
+        this.category = category;
+    }
+
+    public List<ProductDetailDto> getDetails() {
+        return details;
+    }
+
+    public void setDetails(List<ProductDetailDto> details) {
+        this.details = details;
+    }
 }
