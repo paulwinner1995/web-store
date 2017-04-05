bbd91a8a90a3b634b776506bf887bee8409d35bf
ppadalka <ppadalka@inbox.ru>
2017-04-06 01:00:22 +0300

    added parent category name to response

--- src/main/java/ua/ppadalka/webstore/product/mapper/ProductCategoryMapper.java
+++ src/main/java/ua/ppadalka/webstore/product/mapper/ProductCategoryMapper.java
@@ -1,12 +1,17 @@
 package ua.ppadalka.webstore.product.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import ua.ppadalka.webstore.product.dto.ProductCategoryDto;
 import ua.ppadalka.webstore.product.model.ProductCategory;
 
 @Mapper(componentModel = "spring")
 public interface ProductCategoryMapper {
 
+    @Mappings({
+            @Mapping(target = "parentName", source = "parent.name")
+    })
     ProductCategoryDto toDto(ProductCategory productCategory);
 
     ProductCategory toModel(ProductCategoryDto productCategoryDto);
